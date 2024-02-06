449dde6b468cd61a395eb5da1e93fddcf41b8ea1
madhawaucsc2453 <2020cs157@ucsc.cmb.ac.lk>
2024-02-06 21:56:57 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,17 +4,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
      opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
